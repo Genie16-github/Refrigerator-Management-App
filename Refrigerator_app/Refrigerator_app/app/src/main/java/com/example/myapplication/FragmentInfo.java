f75bc5ccc16620c6891171e4dd5c1a1140ceb55e
JaeBeom <4411dlwoqja@naver.com>
2022-11-26 23:58:03 +0900

    update file

--- Refrigerator_app/Refrigerator_app/app/src/main/java/com/example/myapplication/FragmentInfo.java
+++ Refrigerator_app/Refrigerator_app/app/src/main/java/com/example/myapplication/FragmentInfo.java
@@ -60,6 +60,8 @@ public class FragmentInfo extends Fragment {
 
         memo = view.findViewById(R.id.memo);
 
+        storage = "냉장";
+
         birthday = view.findViewById(R.id.button);
         regist = view.findViewById(R.id.button3);
         btn_ref1 = view.findViewById(R.id.storage1);
